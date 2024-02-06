3e1f88ebf62649098bb7fcc739ad9cdc854cd132
EldritchGriffin <abdelhak.form@gmail.com>
2024-02-06 22:50:50 +0100

    removed a line

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {};
 
 module.exports = {
   images: {
-    remotePatterns: ["res.cloudinary.com", "cdn.intra.42.fr"],
     remotePatterns: [
       { hostname: "res.cloudinary.com" },
       { hostname: "cdn.intra.42.fr" },
