0da4aedb976d0a77840a357c47f4d97668c1370a
Anass Bsela <absela@e3r8p18.1337.ma>
2024-02-07 17:28:45 +0100

    nav

--- src/app/(authenticated)/(components)/navbar.tsx
+++ src/app/(authenticated)/(components)/navbar.tsx
@@ -404,8 +404,8 @@ export default function Navbar_compo() {
                                 handlecheck();
                               }}
                             />
-                            <div className="w-14 h-7 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[4px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all dark:border-gray-600 peer-checked:bg-accent_red"></div>
-                            <span className="ms-3 text-lg font-medium  text-white dark:text-gray-300">
+                            <div className="w-14 h-7 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300  rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[4px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all  peer-checked:bg-accent_red"></div>
+                            <span className="ms-3 text-lg font-medium  text-white ">
                               Two Factor Authentication
                             </span>
                           </label>
@@ -497,7 +497,7 @@ export default function Navbar_compo() {
                         handlecheck();
                       }}
                     />
-                    <span className="ms-3 text-xs   text-white dark:text-gray-300">
+                    <span className="ms-3 text-xs   text-white">
                       Two Factor Authentication
                     </span>
                   </label>
